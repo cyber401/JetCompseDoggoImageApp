dc6db50e9110483918775bdec9159c1dee2bdda9
lalit <lalitrajput069@gmail.com>
2021-09-01 11:48:15 +0530

    completed

--- app/src/main/java/com/example/jetcomposesmallproject/MainActivity.kt
+++ app/src/main/java/com/example/jetcomposesmallproject/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -17,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -30,8 +32,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.dog)
-            ImageCard(title = "Doggy", painter)
+            Row(Modifier.fillMaxSize().background(Color.Red),horizontalArrangement = Arrangement.SpaceEvenly ) {
+                ImageCard(title = "Doggy", painterResource(id = R.drawable.dog))
+                ImageCard(title = "android", painterResource(id = R.drawable.android))
+            }
+
         }
     }
 }
@@ -40,19 +45,36 @@ class MainActivity : ComponentActivity() {
 fun ImageCard(title:String, image:Painter, modifier:Modifier = Modifier) {
     Card(
         modifier
-            .fillMaxSize(0.4f)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.4f)
             .padding(4.dp),
         shape = RoundedCornerShape(8.dp),
 
         ) {
+
         Box(modifier.fillMaxSize()){
             Image(painter = image, contentDescription = "nothing", modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop)
         }
+        Box(
+            modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(Color.Transparent, Color.Black),
+                        350f,
+                        1000f
+                    )
+                ))
+
         Box(modifier.fillMaxSize()){
-            Text(text = title, modifier.align(Alignment.BottomCenter),
+            Text(text = title,
+                modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(4.dp),
             color = Color.White)
         }
+
     }
 
 }
